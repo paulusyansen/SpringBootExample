5dcd1585af73b534332c9054ee1cb4c0c0d713ca
paulusyansen <newcurzon@yahoo.com>
2018-07-09 17:09:19 +0700

    add internationalization

--- src/main/java/org/paingan/boot/MainApplication.java
+++ src/main/java/org/paingan/boot/MainApplication.java
@@ -1,5 +1,7 @@
 package org.paingan.boot;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @EnableAutoConfiguration
 @ComponentScan("org.paingan")
@@ -23,6 +28,20 @@ public class MainApplication   extends SpringBootServletInitializer {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+	    SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.US);
+	    return slr;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	    lci.setParamName("lang");
+	    return lci;
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
